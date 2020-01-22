73a6ad56b4376a4bb020063a7e4ee49d58badcc2
Bartosz <bartuskr@gmail.com>
2020-01-22 14:36:15 +0100

    Rename xml for lesson1

--- app/src/main/java/com/pam/elearning/activity/Lesson1.java
+++ app/src/main/java/com/pam/elearning/activity/Lesson1.java
@@ -37,7 +37,7 @@ public class Lesson1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lesson1);
         ButterKnife.bind(this);
 
         setTitle("eLearning - Lesson 1");
