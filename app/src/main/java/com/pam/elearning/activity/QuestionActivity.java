4a2b98927b54885cc40251dc9e8f78bae39670b5
Bartosz <bartuskr@gmail.com>
2020-01-24 13:05:43 +0100

    Initial QuestionActivity

---
+++ app/src/main/java/com/pam/elearning/activity/QuestionActivity.java
@@ -0,0 +1,16 @@
+package com.pam.elearning.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pam.elearning.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
